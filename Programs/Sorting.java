f0bcbb332c08c04e5f927c96731ae4faeaf6ee1c
syamkumardamarla <syamcse5d7@gmail.com>
2024-11-14 20:31:56 -0800

    Sort an Array

---
+++ Programs/Sorting.java
@@ -0,0 +1,22 @@
+
+
+public class Main
+{
+	public static void main(String[] args) {
+		int arr[]={50,40,30,20,10};
+		int i,j;
+		//Bubble sort
+		for( i=0; i<arr.length; i++){
+		    for( j=0; j<arr.length-1-i; j++){
+		        if(arr[j]>arr[j+1]){
+		            int temp=arr[j];
+		            arr[j]=arr[j+1];
+		            arr[j+1]=temp;
+		        }
+		    }
+		}
+		for(i=0; i<arr.length; i++){
+		   System.out.println(arr[i]);
+		    }
+	}
+}
