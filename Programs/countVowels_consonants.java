7f29391dc9a6938105330fdbed40d0d3bb3c1a53
syamkumardamarla <syamcse5d7@gmail.com>
2024-11-15 21:42:45 -0800

    count no of vowels and consonants

---
+++ Programs/countVowels_consonants.java
@@ -0,0 +1,24 @@
+
+import java.util.Scanner;
+public class Main
+{
+    
+    public static void countVowels_consonants(String str){
+        int vowelcount=0,consonantcount=0;
+        for(int i=0; i<str.length(); i++){
+            if(str.charAt(i)=='a' || str.charAt(i)=='e' || str.charAt(i)=='i' || str.charAt(i)=='o' || str.charAt(i)=='u'){
+                vowelcount++;
+            }
+            else{
+                consonantcount++;
+            }
+        }
+        System.out.println("Number of vowels in"+" "+str+":"+vowelcount+" "+"Number of consonats in"+" "+str+":"+consonantcount);
+        
+    }
+	public static void main(String[] args) {
+	    Scanner sc= new Scanner(System.in);
+	    String str= sc.nextLine();
+		countVowels_consonants(str);
+	}
+}
