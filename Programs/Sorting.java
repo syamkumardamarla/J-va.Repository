f54b6c06291d43f8e36b9f9ba73de9fee2384d39
syamkumardamarla <syamcse5d7@gmail.com>
2024-11-15 21:55:58 -0800

    Array in Ascending and Descending order

--- Programs/Sorting.java
+++ Programs/Sorting.java
@@ -3,7 +3,7 @@
 public class Main
 {
 	public static void main(String[] args) {
-		int arr[]={50,40,30,20,10};
+		int arr[]={99,11,33,55,22,77,44,88,66};
 		int i,j;
 		//Bubble sort
 		for( i=0; i<arr.length; i++){
@@ -15,8 +15,15 @@ public class Main
 		        }
 		    }
 		}
+		System.out.println("*******Ascending Order******");
 		for(i=0; i<arr.length; i++){
+		    
 		   System.out.println(arr[i]);
 		    }
+        System.out.println("*******Descending Order******");
+		for(i=arr.length-1; i>0; i--){
+		    
+		   System.out.println(arr[i]);
+		 }
 	}
 }
