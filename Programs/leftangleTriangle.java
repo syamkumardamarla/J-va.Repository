807ecb9495e6d844f6774ccc79de39773b1d1d65
syamkumardamarla <syamcse5d7@gmail.com>
2024-11-16 23:02:28 -0800

    Updated left angle triangle

--- Programs/leftangleTriangle.java
+++ Programs/leftangleTriangle.java
@@ -17,3 +17,42 @@ public class Main
 	    
 	}
 }
+
+
+
+
+public class Main
+{
+    public static void leftangle(int rows){
+        for(int i=0; i<rows; i++){
+            for(int j=0; j<=i; j++){
+                System.out.print(i+1);
+            }
+            System.out.println();
+        }
+        
+    }
+	public static void main(String[] args) {
+		leftangle(5);
+	}
+}
+
+
+
+
+
+public class Main
+{
+    public static void leftangle(int rows){
+        for(int i=0; i<rows; i++){
+            for(int j=0; j<=i; j++){
+                System.out.print(j+1);
+            }
+            System.out.println();
+        }
+        
+    }
+	public static void main(String[] args) {
+		leftangle(5);
+	}
+}
