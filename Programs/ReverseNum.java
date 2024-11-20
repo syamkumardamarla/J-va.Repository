8884ec94dc6a8d4516da9ff31bee1247f87ff5f9
syamkumardamarla <syamcse5d7@gmail.com>
2024-11-20 15:03:20 -0800

    reverse a number

---
+++ Programs/ReverseNum.java
@@ -0,0 +1,15 @@
+public class Main
+{
+    public static void reverseNum(int num){
+        int rev=0,rem;
+        while(num>0){
+            rem= num%10;
+            rev=rev*10+rem;
+            num=num/10;
+        }
+        System.out.println(rev);
+    }
+	public static void main(String[] args) {
+		reverseNum(456);
+	}
+}
