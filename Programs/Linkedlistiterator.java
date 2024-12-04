634aaf5db3b9d9a9ad490ce4e9ba4b37ce243f1b
syamkumardamarla <syamcse5d7@gmail.com>
2024-12-03 18:17:39 -0800

    LinkedList arraylist iterator

--- Programs/Linkedlistiterator.java
+++ Programs/Linkedlistiterator.java
@@ -2,12 +2,12 @@ import java.util.*;
 public class Main
 {
 	public static void main(String[] args) {
-		LinkedList <String> arrlist= new LinkedList<String>();
+		LinkedList <String> linkedlist= new LinkedList<String>();
 		arrlist.add("apple");
 		arrlist.add("Mango");
 		arrlist.add("banana");
 		arrlist.add("grapes");
-		Iterator itr=arrlist.iterator();
+		Iterator itr=linkedlist.iterator();
 		while(itr.hasNext()){
 		    System.out.println(itr.next());
 		}
