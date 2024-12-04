1c0ce40de783a881d14cd827ca1bc04588e73d9a
syamkumardamarla <syamcse5d7@gmail.com>
2024-12-04 10:16:51 -0800

    squares of the array elements

---
+++ Programs/squarearray.java
@@ -0,0 +1,17 @@
+
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		ArrayList <Integer> sqrlist= new ArrayList <Integer>();
+		sqrlist.add(2);
+		sqrlist.add(3);
+		sqrlist.add(4);
+		sqrlist.add(5);
+		sqrlist.add(6);
+		for(int i:sqrlist){
+		    System.out.println(i*i);
+		}
+		
+	}
+}
